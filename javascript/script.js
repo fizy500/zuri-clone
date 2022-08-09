1b3b3c331ecd92a280b5891343a64f54b74f64e3
Otuekong idongesit <otuekongarthur@gmail.com>
2022-08-09 15:22:46 +0100

    Added accordion function to the site

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,7 @@
 // 
+const contentContainer = document.getElementsByClassName('content-container');
+for (i = 0; i < contentContainer.length; i++){
+    contentContainer[i].addEventListener('click',function () {
+        this.classList.toggle('active');
+    })
+};
