8628110b4dc36700e17430780b2428664c5913fd
Otuekong idongesit <otuekongarthur@gmail.com>
2022-08-17 15:24:44 +0100

    Added scrollspy

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,27 @@ for (i = 0; i < contentContainer.length; i++) {
         this.classList.toggle('active');
     })
 };
-
+// Scroll spy
+const sections = document.querySelectorAll('section[id]');
+const navlinks = document.querySelectorAll(' header nav .header-link-container a');
+console.log(navlinks);
+window.onscroll = () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop;
+        const height = sec.offsetHeight;
+        const Id = sec.getAttribute("id");
+        if (
+            top >= offset &&
+            top < offset + height
+        ) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav .header-link-container a[href*=' + Id + ']').classList.add('active');
+            })
+        }
+    });
+}
 // Modal
 const openBtn = document.getElementById('open-modal');
 const modalContainer = document.getElementById('modal-wrapper');
